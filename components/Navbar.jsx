f2d26ff4eca9bd72cc354ad319e8ac093df6b325
ergunDevv <ergncyln2127088@gmail.com>
2022-12-31 14:00:26 +0300

    Product detail page final version

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,26 +1,8 @@
-import React from 'react';
-import Link from 'next/link';
-import { AiOutlineShopping } from 'react-icons/ai'
-
-import { Cart } from './Cart';
-import { useStateContext} from '../context/StateContext';
+import React from 'react'
 
 const Navbar = () => {
-  const { showCart, setShowCart, totalQuantities } = useStateContext();
-
   return (
-    <div className="navbar-container">
-      <p className="logo">
-        <Link href="/">JSM Headphones</Link>
-      </p>
-
-      <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
-        <AiOutlineShopping />
-        <span className="cart-item-qty">{totalQuantities}</span>
-      </button>
-
-      {showCart && <Cart />}
-    </div>
+    <div>Navbar</div>
   )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -7,12 +7,12 @@ import { StateContext } from '../context/StateContext';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <StateContext>
+ 
       <Layout>
         <Toaster />
         <Component {...pageProps} />
       </Layout>
-    </StateContext>
+  
   )
 }
 
