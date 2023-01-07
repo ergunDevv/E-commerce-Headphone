91534019215301edfdf55242972f16c8740560f8
ergunDevv <ergncyln2127088@gmail.com>
2023-01-07 22:32:28 +0300

    Logo and backend changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <div className='navbar-container'>
       <p className='logo'>
-          <Link href="/">Ergun</Link>
+          <Link href="/">Ergun-Headphones</Link>
       </p>
       
       <button type='button' className='cart-icon' onClick={()=>setShowCart(true)}>
