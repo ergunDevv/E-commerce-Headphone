8906b87bd80d11e5f9103206a11cb9c33f8126ce
ergunDevv <ergncyln2127088@gmail.com>
2023-01-22 14:32:16 +0300

    Website title changed

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>JS Mastery Store</title>
+        <title>Ergun-Headphones</title>
       </Head>
       <header>
         <Navbar />
